2da5feb387508f08c41b77556810d99ba656b4c7
Dimitri <Dimitri16h@gmail.com>
2018-06-06 10:17:22 -0400

    Forgot express - added now

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(port, function() {
-  console.log('I am listening on port 3000!');
+app.listen(port, function(){
+	console.log('I am listening on port ' + port + '!');
 });
